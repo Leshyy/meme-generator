fe9a60acf746a0e8bcc957dda05226ce9510f4a1
Tamir Leshetz <tamir.leshetz@gmail.com>
2020-12-09 16:23:07 +0200

    gallery with img rendering no style

--- js/meme-contoller.js
+++ js/meme-contoller.js
@@ -4,28 +4,33 @@ let gCanvas;
 let gCtx;
 
 function onInit() {
+    // hide meme editor page
+    document.querySelector('.meme-editor').classList.add('none');
+
     gCanvas = document.querySelector('#canvas');
     gCtx = gCanvas.getContext('2d');
     renderMeme();
+    renderGallery();
+}
+
+function renderGallery() {
+    let imgStrHTML = '';
+    const imgs = getImgs();
+    imgs.forEach((img) => {
+        imgStrHTML += `<img onClick="onImgClick(${img.id})" src="${img.url}" />`;
+    });
+    document.querySelector('.gallery-grid').innerHTML = imgStrHTML;
 }
 
 function renderMeme() {
     const currMeme = getMeme();
-    const url = currMeme.selectedImg.url;
+    const url = getImgURL();
     const img = new Image();
     img.src = url;
     img.onload = () => {
         gCtx.drawImage(img, 0, 0);
-        drawText(
-            currMeme.lines[0].txt,
-            currMeme.lines[0].x,
-            currMeme.lines[0].y
-        );
-        drawText(
-            currMeme.lines[1].txt,
-            currMeme.lines[1].x,
-            currMeme.lines[1].y
-        );
+        drawText(getMemeText(0), currMeme.lines[0].x, currMeme.lines[0].y);
+        drawText(getMemeText(1), currMeme.lines[1].x, currMeme.lines[1].y);
     };
 }
 
@@ -36,4 +41,33 @@ function drawText(text, x, y) {
     gCtx.textAlign = 'center';
     gCtx.fillText(text, x, y);
     gCtx.strokeText(text, x, y);
+}
+
+function onTextChange(txt) {
+    setMemeText(txt.toUpperCase());
+    renderMeme();
+}
+function onSwitchLines() {
+    document.querySelector('#meme-txt').value = '';
+    switchSelectedLine();
+}
+
+function onImgClick(id) {
+    setMeme(id);
+    renderMeme();
+    hideGallery();
+    showEditMeme();
+}
+
+function hideGallery() {
+    document.querySelector('.img-gallery').classList.add('none');
+}
+function showGallery() {
+    document.querySelector('.img-gallery').classList.remove('none');
+}
+function showEditMeme() {
+    document.querySelector('.meme-editor').classList.remove('none');
+}
+function hideEditMeme() {
+    document.querySelector('.meme-editor').classList.add('none');
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -3,6 +3,22 @@
 const gImgs = [
     { id: 1, url: 'imgs/1.jpg', keywords: ['mad'] },
     { id: 2, url: 'imgs/2.jpg', keywords: ['happy'] },
+    { id: 3, url: 'imgs/3.jpg', keywords: ['happy'] },
+    { id: 4, url: 'imgs/4.jpg', keywords: ['happy'] },
+    { id: 5, url: 'imgs/5.jpg', keywords: ['happy'] },
+    { id: 6, url: 'imgs/6.jpg', keywords: ['happy'] },
+    { id: 7, url: 'imgs/7.jpg', keywords: ['happy'] },
+    { id: 8, url: 'imgs/8.jpg', keywords: ['happy'] },
+    { id: 9, url: 'imgs/9.jpg', keywords: ['happy'] },
+    { id: 10, url: 'imgs/10.jpg', keywords: ['happy'] },
+    { id: 11, url: 'imgs/11.jpg', keywords: ['happy'] },
+    { id: 12, url: 'imgs/12.jpg', keywords: ['happy'] },
+    { id: 13, url: 'imgs/13.jpg', keywords: ['happy'] },
+    { id: 14, url: 'imgs/14.jpg', keywords: ['happy'] },
+    { id: 15, url: 'imgs/15.jpg', keywords: ['happy'] },
+    { id: 16, url: 'imgs/16.jpg', keywords: ['happy'] },
+    { id: 17, url: 'imgs/17.jpg', keywords: ['happy'] },
+    { id: 18, url: 'imgs/118.jpg', keywords: ['happy'] },
 ];
 
 let gMeme = {
@@ -39,4 +55,30 @@ function findImg(id) {
         return img.id === id;
     });
     return img;
+}
+// toggles the active line
+function switchSelectedLine() {
+    if (gMeme.selectedLineIdx === 0) {
+        gMeme.selectedLineIdx++;
+    } else gMeme.selectedLineIdx = 0;
+}
+
+function getImgURL() {
+    return gMeme.selectedImg.url;
+}
+function getMemeText(idx) {
+    return gMeme.lines[idx].txt;
+}
+
+function setMemeText(txt) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = txt;
+}
+
+function setMemeImg(img) {
+    gMeme.selectedImg = img;
+}
+
+function setMeme(id) {
+    const imgSelected = findImg(id);
+    setMemeImg(imgSelected);
 }
